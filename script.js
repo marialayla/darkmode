ea5b44cbca687cc78a146220897c7fc8b132492e
marialayla <138811433+marialayla@users.noreply.github.com>
2023-11-16 18:24:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function darkmode(){
+    const pagina = document.body
+
+    pagina.classList.toggle("dark-mode")
+
+    const botao = document.querySelector("button")
+    const isDarkMode = document.body.classList.contains("dark-mode");
+
+
+    if(isDarkMode  == true){
+
+     botao.textContent = "Modo Claro"
+    }else{
+        botao.textContent = "Modo Escuro"
+        
+    }
+
+}
